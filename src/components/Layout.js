e06123515279a8478ca295ad306fcb5cbc07d0fc
SenilViradiya <146502747+SenilViradiya@users.noreply.github.com>
2025-03-06 15:57:17 +0530

    Update Layout.js to impliment google adds

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,4 +13,13 @@ export default function Layout({ children }) {
     </div>
   )
 }
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-16911547777"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-16911547777');
+</script>
 
